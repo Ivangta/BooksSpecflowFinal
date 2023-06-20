2470611797fe2ace8e991300ddd4d8a424e37aa6
Ivan G <ivanntu8@gmail.com>
2023-06-20 17:46:05 +0300

    Login

--- StepDefinitions/LoginSteps.cs
+++ StepDefinitions/LoginSteps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BooksSpecflow.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,35 @@ using System.Threading.Tasks;
 
 namespace BooksSpecflow.StepDefinitions
 {
-    internal class LoginSteps
+    [Binding]
+    public class LoginSteps
     {
+        private readonly BaseUserActions _user;
+        private readonly ScenarioContext _scenarioContext;
+
+        public LoginSteps(BaseUserActions user, ScenarioContext scenarioContext)
+        {
+            _user = user;
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"Homepage is open")]
+        public void GivenHomepageIsOpen()
+        {
+            _user.OpensPage("https://qa-task.immedis.com/");
+        }
+
+        [When(@"I log in admin and (.*)")]
+        public void WhenILogInAdminAnd(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I am successfully logged in")]
+        public void ThenIAmSuccessfullyLoggedIn()
+        {
+            throw new PendingStepException();
+        }
+
     }
 }
---
+++ Utils/BaseUserActions.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.WaitHelpers;
+
+namespace BooksSpecflow.Utils
+{
+    public class BaseUserActions
+    {
+        protected IWebDriver _currentPage;
+        private WebDriverWait _wait;
+
+        public BaseUserActions()
+        {
+            _wait = new WebDriverWait(_currentPage, TimeSpan.FromSeconds(2));
+        }
+
+        internal void OpensPage(string pageName)
+        {
+            _currentPage = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _currentPage.Manage().Window.Maximize();
+            _wait = new WebDriverWait(_currentPage, TimeSpan.FromSeconds(2));
+            _currentPage.Navigate().GoToUrl(pageName);
+            _wait = new WebDriverWait(_currentPage, TimeSpan.FromSeconds(2));
+        }
+
+        internal IWebElement WaitsUntilClickable(By elementLocator)
+        {
+            return _wait.Until(ExpectedConditions.ElementToBeClickable(elementLocator));
+        }
+
+        internal IWebElement WaitsUntilVisible(By elementLocator)
+        {
+            return _wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+        }
+
+        internal void WaitUntilNotVisible(By elementLocator)
+        {
+            _wait.Until(ExpectedConditions.InvisibilityOfElementLocated(elementLocator));
+        }
+
+        internal void WaitUntilNotVisible(By elementLocator, TimeSpan timeout)
+        {
+            TimeSpan originalTimeout = _wait.Timeout;
+            _wait.Timeout = timeout;
+
+            _wait.Until(ExpectedConditions.InvisibilityOfElementLocated(elementLocator));
+
+            _wait.Timeout = originalTimeout;
+        }
+
+        internal void ClicksOn(By elementLocator)
+        {
+            WaitsUntilClickable(elementLocator).Click();
+        }
+
+        internal string ReadsTextFrom(By elementLocator)
+        {
+            return WaitsUntilVisible(elementLocator).Text.Trim();
+        }
+
+    }
+}
