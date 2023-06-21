36c4ef0dab00c8048d7e4cad5f2ce6052d30463b
Ivan G <ivanntu8@gmail.com>
2023-06-21 14:18:28 +0300

    Rest client added

---
+++ URestSharp/RestFunctions.cs
@@ -0,0 +1,28 @@
+﻿using BooksSpecflow.URestModels.Requests;
+using BooksSpecflow.Utils;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSpecflow.URestSharp
+{
+    internal class RestFunctions
+    {
+        private Helpers helper;
+        public RestFunctions()
+        {
+            helper = new Helpers();
+        }
+        public async Task<RestResponse> CreateNewUser(string baseUrl, dynamic payload)
+        {
+            var client = helper.SetUrl(baseUrl, "api/users");
+            //var jsonString = HandleContent.SerializeJsonString(payload);
+            var request = helper.CreatePostRequest<CreateUserReq>(payload);
+            var response = await helper.GetResponseAsync(client, request);
+            return response;
+        }
+    }
+}
--- Utils/Helpers.cs
+++ Utils/Helpers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace BooksSpecflow.Utils
 {
     public class Helpers
     {
+        private RestClient client;
+        private RestRequest request;
         public static string GenerateRandomAlphanumericString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -22,6 +25,73 @@ namespace BooksSpecflow.Utils
         public static string RemoveNonNumericValuesUsingRegex(string source)
         {
             return Regex.Replace(source, "[^\\d.]", string.Empty);
+        }
+
+        public RestClient SetUrl(string baseUrl, string endpoint)
+        {
+            var url = Path.Combine(baseUrl, endpoint);
+            client = new RestClient(url);
+            //client.ClientCertificates = new X509CertificateCollection() { certificate };
+            //client.Proxy = new WebProxy();
+            return client;
+        }
+
+        public RestRequest CreateGetRequest()
+        {
+            request = new RestRequest()
+            {
+                Method = Method.Get
+            };
+            //request.AddHeader("Accept", "application/json");
+            request.AddHeaders(new Dictionary<string, string>
+            {
+                { "Accept", "application/json" },
+                { "Content-Type", "application/json" }
+
+            });
+            //request.AddFile("Test file", @"C:\Users\yadavm\Downloads\Test.txt", "multipart/form-data");
+            return request;
+        }
+
+        public RestRequest CreatePostRequest<T>(T payload) where T : class
+        {
+            request = new RestRequest()
+            {
+                Method = Method.Post
+            };
+            request.AddHeader("Accept", "application/json");
+            //request.AddParameter("application/json", payload, ParameterType.RequestBody);
+            request.AddBody(payload);
+            request.RequestFormat = DataFormat.Json;
+            return request;
+        }
+
+        public RestRequest CreatePutRequest<T>(T payload) where T : class
+        {
+            request = new RestRequest()
+            {
+                Method = Method.Put
+            };
+            request.AddHeader("Accept", "application/json");
+            //request.AddParameter("application/json", payload, ParameterType.RequestBody);
+            request.AddBody(payload);
+            request.RequestFormat = DataFormat.Json;
+            return request;
+        }
+
+        public RestRequest CreateDeleteRequest()
+        {
+            request = new RestRequest()
+            {
+                Method = Method.Delete
+            };
+            request.AddHeader("Accept", "application/json");
+            return request;
+        }
+
+        public async Task<RestResponse> GetResponseAsync(RestClient restClient, RestRequest restRequest)
+        {
+            return await restClient.ExecuteAsync(restRequest);
         }
     }
 }
